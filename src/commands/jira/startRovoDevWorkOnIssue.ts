ae827b7cf5d2f37a19eea24a9a7910140c5b9424
amarg-at <156738553+amarg-at@users.noreply.github.com>
2025-08-02 13:55:20 +1000

    basic command entry in context menu

---
+++ src/commands/jira/startRovoDevWorkOnIssue.ts
@@ -0,0 +1,28 @@
+import { isMinimalIssue, MinimalIssue, MinimalIssueOrKeyAndSite } from '@atlassianlabs/jira-pi-common-models';
+
+import { DetailedSiteInfo } from '../../atlclients/authInfo';
+import { Container } from '../../container';
+import { fetchMinimalIssue } from '../../jira/fetchIssue';
+import { FeatureFlagClient, Features } from '../../util/featureFlags';
+
+export async function startRovoDevWorkOnIssue(issueOrKeyAndSite: MinimalIssueOrKeyAndSite<DetailedSiteInfo>) {
+    let issue: MinimalIssue<DetailedSiteInfo>;
+
+    if (isMinimalIssue(issueOrKeyAndSite)) {
+        issue = issueOrKeyAndSite;
+    } else {
+        issue = await fetchMinimalIssue(issueOrKeyAndSite.key, issueOrKeyAndSite.siteDetails);
+
+        if (!issue) {
+            throw new Error(`Jira issue ${issueOrKeyAndSite.key} not found in site ${issueOrKeyAndSite.siteDetails}`);
+        }
+    }
+
+    const { startWorkV3WebviewFactory, startWorkWebviewFactory } = Container;
+
+    const factory = FeatureFlagClient.checkGate(Features.StartWorkV3)
+        ? startWorkV3WebviewFactory
+        : startWorkWebviewFactory;
+
+    factory.createOrShow({ issue });
+}
